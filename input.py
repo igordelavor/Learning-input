31b3cd368666714c2e25da91642300345539eb3e
igordelavor <60244036+igordelavor@users.noreply.github.com>
2020-02-10 11:21:49 -0300

    Add files via upload

---
+++ input.py
@@ -0,0 +1,24 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+def teste():
+    numero = int(input("Digite um numero: "))
+    if(numero == 1):
+        print("deu certo")
+    else:
+        print("deu errado")
+def teste01():
+    print("Digite um numero:")
+    numero = int(input())
+    if(numero == 1):
+        print("deu certo")
+    else:
+        print("deu errado")
+    
+
+
+

38e7ddb934827877c22d2fc5b5a0f7c1db5f3403
igordelavor <60244036+igordelavor@users.noreply.github.com>
2020-02-17 17:41:13 -0500

    Add files via upload

---
+++ binary_to_decimal.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[16]:
+
+
+def converter():
+    s = str(input("Digite um numero binário: "))
+    x = int(s, 2)
+    print('Numero em decimal:',x)
+converter()
+
+
+# In[ ]:
+
+
+
+

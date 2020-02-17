070227f5287289a62de032a5e8d3aa826c0ca3fd
igordelavor <60244036+igordelavor@users.noreply.github.com>
2020-02-17 17:41:32 -0500

    Add files via upload

---
+++ name_and_age.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+def exercicio():
+    name = str(input('Digite seu nome: '))
+    idade = int(input('Digite sua idade: '))
+    x = 100 - idade
+    print(name, 'voceh completarah 100 anos em:',x,'anos')
+exercicio()
+
+
+# In[ ]:
+
+
+
+
